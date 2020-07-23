6dbefe32ecba6f08ee36c5596cf08d96452caf2e
PC-SANTIAGO\Santiago <souto.santiago.mattias@gmail.com>
2020-07-23 20:38:22 -0300

    Utils file for define in-game actions.

---
+++ backend/utils.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+import random
+
+def tirarDado(ins,dic):
+    n = int(ins.split("d")[0])
+    caras = int(ins.split("d")[1])            
+    for i in range(1,n+1):
+        tirada = random.randint(1,caras)
+        dic[str(i)+"d"+str(caras)] = tirada        
+    return dic
+
+
+def sumDices(instrucciones):
+    lista = []
+    dic = {}
+    for elemento in instrucciones:        
+        cantidad = int(elemento.split("d")[0])        
+        caras = int(elemento.split("d")[1])        
+        if caras not in dic.keys():
+            dic[caras] = cantidad
+        else:
+            dic[caras]+=cantidad    
+    for key in dic:
+        lista.append(str(dic[key])+"d"+str(key))
+    return lista
+        
+def flipCoin(n):
+	lista = []
+	for n in range(1, n+1):
+		valor = random.randint(1, 2)
+		opcion = int(valor)
+		if opcion == 1:
+			lista.append("True")
+		else:
+			lista.append("False")
+	return lista
